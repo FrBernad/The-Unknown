aac341ab403d75d676dde9475dda20f1f9f4b316
frbernad <frbernad@itba.edu.ar>
2022-10-11 17:26:27 -0300

    feat: add notes around the map

--- Assets/Scripts/NotesSpawner.cs
+++ Assets/Scripts/NotesSpawner.cs
@@ -9,7 +9,7 @@ using SystemRandom = System.Random;
 public class NotesSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _cubePrefab;
-    [SerializeField] private List<Transform> _spawnPoints;
+    [SerializeField] private List<GameObject> _spawnPoints;
 
     [SerializeField] private int _totalNotes = 4;
 
@@ -24,14 +24,18 @@ public class NotesSpawner : MonoBehaviour
         for (var i = 0; i < _totalNotes; i++)
         {
             var index = Random.Range(0, _spawnPoints.Count);
-            Instantiate(_cubePrefab, _spawnPoints[index].position, Quaternion.identity);
             _spawnPoints.RemoveAt(index);
         }
     }
 
     private void SetSpawnPoints()
     {
-        GameObject spawnPoints = GameObject.Find("NotesSpawnPoints");
+        GameObject lightHousePoints = GameObject.Find("NotesSpawnPoints");
+        GameObject housePoints = GameObject.Find("NotesSpawnPoints");
+        GameObject backLeftPoints = GameObject.Find("NotesSpawnPoints");
+        GameObject centerPoints = GameObject.Find("NotesSpawnPoints");
+        GameObject rightPoints = GameObject.Find("NotesSpawnPoints");
+
         int totalSpawnPoints = spawnPoints.transform.childCount;
 
         _spawnPoints = new List<Transform>(totalSpawnPoints);
