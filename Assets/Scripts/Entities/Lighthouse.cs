0d148d08fea5d64f64efd0551e8d86163824712c
frbernad <frbernad@itba.edu.ar>
2022-12-07 16:50:26 -0300

    feat: lighthouse target follow

--- Assets/Scripts/Entities/Lighthouse.cs
+++ Assets/Scripts/Entities/Lighthouse.cs
@@ -1,5 +1,9 @@
 using System;
 using UnityEngine;
+using UnityEngine.Scripting;
+using static System.Math;
+using static UnityEngine.Mathf;
+using Object = UnityEngine.Object;
 
 namespace Entities
 {
@@ -7,25 +11,48 @@ namespace Entities
     {
         [SerializeField] private GameObject _pointLight;
         [SerializeField] private GameObject _spotLight;
+        [SerializeField] private Character _character;
 
         [SerializeField] private float _speed = 0.1F;
         [SerializeField] private float _radius = 3;
+        [SerializeField] private float _offset = 90;
 
         private float _currentTime;
 
         private void Update()
         {
-            _currentTime += Time.deltaTime;
+            TargetPlayer();
+            // NormalRotation();
+        }
 
-            var localPosition = _pointLight.transform.localPosition;
+        private void NormalRotation()
+        {
+            _spotLight.transform.Rotate(Vector3.up * Rad2Deg * (Time.deltaTime * _speed));
 
-            var x = (float)Math.Cos(-_currentTime * _speed) * _radius;
-            var y = localPosition.y;
-            var z = (float)Math.Sin(-_currentTime * _speed) * _radius;
+            var pointLightPosition = _pointLight.transform.localPosition;
+
+            var rotation = _spotLight.transform.localRotation.eulerAngles.y - _offset;
+            var x = Cos(-Deg2Rad * rotation) * _radius;
+            var y = pointLightPosition.y;
+            var z = Sin(-Deg2Rad * rotation) * _radius;
 
             _pointLight.transform.localPosition = new Vector3(x, y, z);
+        }
 
-            _spotLight.transform.Rotate(Vector3.up * Mathf.Rad2Deg * (Time.deltaTime * _speed));
+        private void TargetPlayer()
+        {
+            var direction = _character.transform.position - _spotLight.transform.position;
+            var targetRotation = Quaternion.LookRotation(direction);
+            var spotlightTargetRotation = _spotLight.transform.rotation;
+            spotlightTargetRotation = Quaternion.Slerp(spotlightTargetRotation, targetRotation, Time.deltaTime);
+            _spotLight.transform.rotation = spotlightTargetRotation;
+
+            var rotation = _spotLight.transform.localRotation.eulerAngles.y - _offset;
+            var pointLightPosition = _pointLight.transform.localPosition;
+            var x = Cos(-Deg2Rad * rotation) * _radius;
+            var y = pointLightPosition.y;
+            var z = Sin(-Deg2Rad * rotation) * _radius;
+            _pointLight.transform.localPosition = new Vector3(x, y, z);
         }
     }
 }
